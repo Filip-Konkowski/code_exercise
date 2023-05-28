fecbbea02599ef16c337a917ccdcf3a67cf2c939
filipkonkowski <filip.konkowski@icabbi.com>
2023-05-28 09:00:37 +0100

    sol2

---
+++ productExceptSelf/src/test/kotlin/Solution2Test.kt
@@ -0,0 +1,43 @@
+import org.junit.Test
+import kotlin.test.assertEquals
+
+class Solution2Test {
+    @Test
+    fun `check valid sudoku`() =
+
+        assertProductEquals(
+            arrayOf(
+                charArrayOf('5','3','.','.','7','.','.','.','.'),
+                charArrayOf('6','.','.','1','9','5','.','.','.'),
+                charArrayOf('.','9','8','.','.','.','.','6','.'),
+                charArrayOf('8','.','.','.','6','.','.','.','3'),
+                charArrayOf('4','.','.','8','.','3','.','.','1'),
+                charArrayOf('7','.','.','.','2','.','.','.','6'),
+                charArrayOf('.','6','.','.','.','.','2','8','.'),
+                charArrayOf('.','.','.','4','1','9','.','.','5'),
+                charArrayOf('.','.','.','.','8','.','.','7','9'),
+            ),
+            true,
+        )
+
+    @Test
+    fun `check in valid sudoku`() =
+
+        assertProductEquals(
+            arrayOf(
+                charArrayOf('8','3','.','.','7','.','.','.','.'),
+                charArrayOf('6','.','.','1','9','5','.','.','.'),
+                charArrayOf('.','9','8','.','.','.','.','6','.'),
+                charArrayOf('8','.','.','.','6','.','.','.','3'),
+                charArrayOf('4','.','.','8','.','3','.','.','1'),
+                charArrayOf('7','.','.','.','2','.','.','.','6'),
+                charArrayOf('.','6','.','.','.','.','2','8','.'),
+                charArrayOf('.','.','.','4','1','9','.','.','5'),
+                charArrayOf('.','.','.','.','8','.','.','7','9'),
+            ),
+            false,
+        )
+}
+
+private fun assertProductEquals(origin: Array<CharArray>, result: Boolean) = assertEquals(result, Solution2.isValidSudoku(origin))
+
