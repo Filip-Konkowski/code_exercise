4e6c1187624b67b46612de5e53eac4391e42b2f9
filipkonkowski <filip.konkowski@icabbi.com>
2023-06-14 16:52:05 +0100

    Solution 3

--- productExceptSelf/src/main/kotlin/Solution3.kt
+++ productExceptSelf/src/main/kotlin/Solution3.kt
@@ -1,20 +1,19 @@
 object Solution3 {
     fun longestConsecutive(nums: IntArray): Int {
-        val setNums = setOf(nums)
+        val setNums = nums.toSet()
         var longest = 0
-        println(setNums)
+
         for (num in nums) {
             // check if left side exists, so we will have start of sequence
             val leftNum: Int = num - 1
-//            if (setNums.contains(leftNum)) {
-//                var currentLongest = 0
-//                println("here we go")
-//            }
-
-
+            if (!setNums.contains(leftNum)) {
+                var currentLongest = 1
+                while(setNums.contains(num + currentLongest)) {
+                    currentLongest++
+                }
+                longest = maxOf(currentLongest, longest)
+            }
         }
-
-
-        return 1
+        return longest
     }
 }
