802e563b980dab9ed2f5469837b6ac2db5760900
filipkonkowski <filip.konkowski@icabbi.com>
2023-06-19 10:06:02 +0200

    Single Number finder with implementation

--- productExceptSelf/src/main/kotlin/Solution5.kt
+++ productExceptSelf/src/main/kotlin/Solution5.kt
@@ -3,6 +3,15 @@ import java.util.PriorityQueue
 object Solution5 {
     fun singleNumber(nums: IntArray): Int
     {
-        return 1
+        val list = mutableListOf<Int>()
+        for (num in nums) {
+            if(!list.contains(num)) {
+                list.add(num)
+            } else {
+                list.remove(num)
+            }
+        }
+
+        return list.get(0)
     }
 }
