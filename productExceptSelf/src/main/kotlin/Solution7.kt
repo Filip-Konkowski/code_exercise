d17813b3fcf155c99aa3c42ae065337f9b1343e4
filipkonkowski <filip.konkowski@icabbi.com>
2023-06-26 13:29:18 +0200

    Implement sol7

---
+++ productExceptSelf/src/main/kotlin/Solution7.kt
@@ -0,0 +1,27 @@
+import java.util.PriorityQueue
+
+object Solution7 {
+    fun isPalindrome(s: String): Boolean
+    {
+        var left = 0
+        var right = s.length - 1
+        while (left < right) {
+            while (left < right && !s[left].isLetterOrDigit()) {
+                left++
+            }
+
+            while (left < right && !s[right].isLetterOrDigit()) {
+                right--
+            }
+
+            if (s[right].toLowerCase() != s[left].toLowerCase()) {
+                return false
+            }
+
+            left++
+            right--
+        }
+
+        return true
+    }
+}
